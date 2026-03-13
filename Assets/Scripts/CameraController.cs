5d2a26dcadf180fd52c1855bd1dcbb88c5a9608a
DiegoDiaz2704 <diegoa.diazr@konradlorenz.edu.co>
2026-03-13 14:08:03 -0500

    Add files via upload

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject player;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;  //transform.position obtiene la posición del objeto
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate (){
+        transform.position = player.transform.position + offset; //Se ejecuta por cada frame del juego, este método sirve para hacer que la cámara siga al jugador y siempre mantenga la misma distancia con respecto al jugador 
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody rb;
+    public float speed;
+    private Vector3 posicion;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody> ();//Solicita a Unity que devuelva el componente especifico de un objeto, en este caso el componenete Rigidbody
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movimiento = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movimiento * speed);
+
+    }
+    
+}
