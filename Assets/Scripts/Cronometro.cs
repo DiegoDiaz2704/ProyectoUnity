7bc7ea0c14ce3f1cf6c2ff6c3a7ddcc871ba596a
JaimeNovoa <jestebann@hotmail.com>
2026-03-13 16:46:00 -0500

    Cronometro y reinicio
    Agrego el cronometro en pantalla, al detención de este al llegar a la meta (plano azul) y su reinicio al llegar a la plataforma inicial.
    Adicionalmente agrego el reinicio del jugador y el cronometro cuando la esfera se cae del mapa.

---
+++ Assets/Scripts/Cronometro.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Cronometro : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI textoCrono; //Identificar texto dentro del componente
+    [SerializeField] private float tiempo;//Tiempo en segundos
+    [SerializeField] TextMeshProUGUI felicitacion;
+    private int minutos, segundos, decimasSegundo;
+    private bool circuitoFinalizado = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!circuitoFinalizado)
+        {
+            cronometro();
+        }
+    }
+
+    void cronometro()
+    {
+        tiempo += Time.deltaTime;
+
+        minutos = Mathf.FloorToInt(tiempo / 60);
+        segundos = Mathf.FloorToInt(tiempo % 60);
+        decimasSegundo = Mathf.FloorToInt((tiempo % 1) * 100);
+
+        //Mathf.FloorToInt se usa para mostrar el entero
+
+        textoCrono.text = string.Format("{0:00}:{1:00}:{2:00}", minutos, segundos, decimasSegundo);
+    }//Fin CRONOMETRO
+
+    public void detenerTiempo()
+    {
+
+        circuitoFinalizado = true;
+        felicitacion.text = "¡Felicidades!\nTu tiempo fue: " + textoCrono.text;
+        felicitacion.gameObject.SetActive(true);
+
+    } //Fin DETENER_TIEMPO
+
+    public void reiniciarCrono()
+    {
+        textoCrono.text = string.Format("{0:00}:{1:00}:{2:00}", 0, 0, 0);
+        tiempo = 0;
+        circuitoFinalizado = false;
+        felicitacion.gameObject.SetActive(false);
+
+    }//Fin REINICIAR_CRONO
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,18 +10,25 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     public float speed;
     private Vector3 posicion;
+    private Cronometro crono;
+    private Vector3 posicionInicial;
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody> ();//Solicita a Unity que devuelva el componente especifico de un objeto, en este caso el componenete Rigidbody
+        crono = FindObjectOfType<Cronometro>();//Obtener el componente Cronometro de manera automatica, FindObjectOfType busca en toda la escena coincidencia con <>
+        posicionInicial = transform.position;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < 7)
+        {
+            reiniciarJugador();
+        }
     }
 
     void FixedUpdate()
@@ -34,5 +41,23 @@ public class PlayerController : MonoBehaviour
         rb.AddForce (movimiento * speed);
 
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Meta"))
+        {
+            crono.detenerTiempo();
+        }else if (other.CompareTag("Reinicio"))
+        {
+            crono.reiniciarCrono();
+        }
+    }
+
+    void reiniciarJugador()
+    {
+        rb.velocity = Vector3.zero; //rb.velocity modifica unicamente la velocidad, Vector3.zero vector con componentes(0.0.0)
+        transform.position = posicionInicial;
+        crono.reiniciarCrono();
+    }//Fin REINICIAR_JUGADOR
     
 }
